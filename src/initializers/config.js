e31e048da518761807efcb2b026b538ef0c35d60
Gil Mendes <gil00mendes@gmail.com>
2015-12-28 22:18:55 +0000

    Add initialiser to read projects configurations

--- src/initializers/config.js
+++ src/initializers/config.js
@@ -1,6 +1,25 @@
+import fs from 'fs';
+
 export default class Config {
 
+  static loadPriority = 0;
+
   static load(api, next) {
+    // set config object on API
+    api.config = {};
+
+    try {
+      // read project manifest
+      api.config.app = require(`${api.scope.rootPath}/manifest.json`);
+    } catch (e) {
+      // when the project manifest doesn't exists the user is informed
+      // and the engine instance is terminated
+      api.log.emergency('Project `manifest.json` file does not exists.');
+
+      // end the engine execution
+      api.shutdown(true);
+    }
+
     next();
   }
 
