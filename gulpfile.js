a5ca14e92d9660737e761169b26b8c5a8fa33caf
Gil Mendes <gil00mendes@gmail.com>
2015-12-28 11:02:30 +0000

    Add EsLint to the gulp script
    This is not used on watch because eslint not yet support class properties

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var notify = require('gulp-notify');
+var eslint = require('gulp-eslint');
 var sourcemaps = require('gulp-sourcemaps');
 
 /**
@@ -23,6 +24,23 @@ gulp.task('babel', function (cb) {
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(destinationDir))
     .on('end', cb);
+});
+
+/**
+ * Lint JavaScript files using eslint tool.
+ */
+gulp.task('lint', function () {
+  // Return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src('src/**/*.js')
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint result to the console.
+    .pipe(eslint.format())
+    // to have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError() last.
+    .pipe(eslint.failAfterError());
 });
 
 /**
