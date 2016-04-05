74b4238f8e0e935ab4eed62bd6375da20adabfa4
Gil Mendes <gil00mendes@gmail.com>
2016-04-05 20:17:27 +0100

    improve HTTP routes implementation

--- src/initializers/routes.js
+++ src/initializers/routes.js
@@ -1,203 +1,251 @@
+import fs from 'fs'
 import Utils from '../utils';
 
+/**
+ * Class to manage the HTTP action routes.
+ */
 class RoutesManager {
 
   /**
-   * API instance.
+   * API reference.
+   *
+   * @type {null}
    */
-  api;
+  api = null
 
   /**
-   * Hash with the registered routes.
+   * Map with the registered routes.
    *
    * @type {{}}
    */
-  routes = {};
+  routes = {'get': [], 'post': [], 'put': [], 'patch': [], 'delete': []}
 
   /**
    * Available verbs.
    *
    * @type {string[]}
    */
-  verbs = [ 'get', 'post', 'put', 'patch', 'delete' ];
+  verbs = [ 'get', 'post', 'put', 'patch', 'delete' ]
 
   /**
    * Create a new RoutesManager instance.
    *
-   * @param api API object.
+   * @param api API reference.
    */
   constructor(api) {
-    let self = this;
+    let self = this
 
-    self.api = api;
+    // save the API object reference
+    self.api = api
   }
 
+  /**
+   * Process a route call.
+   *
+   * @param connection    Connection object.
+   * @param pathParts     URI parts.
+   */
   processRoute(connection, pathParts) {
-    let self = this;
+    let self = this
 
     // check if the connection contains an action and that action are defined on the current context
     if (connection.params.action === undefined || self.api.actions.actions[ connection.params.action ] === undefined) {
-      // get request method
-      let method = connection.rawConnection.method.toLowerCase();
+      // get HTTP request method
+      let method = connection.rawConnection.method.toLowerCase()
 
       // if its a 'head' request change it to a 'get'
-      if (method === 'head' && !self.routes.head) {
-        method = 'get';
-      }
+      if (method === 'head' && !self.routes.head) { method = 'get' }
 
       // iterate all registered routes
       for (let i in self.routes[ method ]) {
-        let route = self.routes[ method ][ i ];
+        let route = self.routes[ method ][ i ]
 
         // check if exists an URL match
         let match = self.matchURL(pathParts, route.path, route.matchTrailingPathParts);
 
         if (match.match === true) {
           if (route.apiVersion) {
-            connection.params.apiVersion = connection.param.apiVersion || route.apiVersion;
+            connection.params.apiVersion = connection.param.apiVersion || route.apiVersion
           }
 
           // decode URL params
           for (let param in match.params) {
             try {
-              let decodedName = decodeURIComponent(param.replace(/\+/g, ' '));
-              let decodedValue = decodeURIComponent(match.params[ param ].replace(/\+g/, ' '));
-              connection.params[ decodedName ] = decodedValue;
+              let decodedName = decodeURIComponent(param.replace(/\+/g, ' '))
+              let decodedValue = decodeURIComponent(match.params[ param ].replace(/\+g/, ' '))
+              connection.params[ decodedName ] = decodedValue
             } catch (e) {
               // malformed URL
             }
           }
 
           // put the action in the connection
-          connection.params.action = route.action;
-          break;
+          connection.params.action = route.action
+          break
         }
       }
     }
   }
 
+  /**
+   * Check if the url match with parts.
+   *
+   * @param pathParts               URL parts to check.
+   * @param match                   Route URL to check with.
+   * @param matchTrailingPathParts  Check the existence of the path in any part of the URL.
+   * @returns {{match: boolean, params: {}}}
+   */
   matchURL(pathParts, match, matchTrailingPathParts) {
-    let response = {match: false, params: {}};
-    let matchParts = match.split('/');
-    let regexp = '';
-    let variable = '';
+    let response = {match: false, params: {}}
+    let matchParts = match.split('/')
+    let regexp = ''
+    let variable = ''
 
-    if (matchParts[ 0 ] === '') {
-      matchParts.splice(0, 1);
-    }
+    if (matchParts[ 0 ] === '') { matchParts.splice(0, 1) }
 
-    if (matchParts[ (matchParts.length - 1) === '' ]) {
-      matchParts.pop();
-    }
+    if (matchParts[ (matchParts.length - 1) === '' ]) { matchParts.pop() }
 
-    if (matchParts.length !== pathParts.length && matchTrailingPathParts !== true) {
-      return response;
-    }
+    if (matchParts.length !== pathParts.length && matchTrailingPathParts !== true) { return response }
 
     for (let i in matchParts) {
-      let matchPart = matchParts[ i ];
-      let pathPart = pathParts[ i ];
+      let matchPart = matchParts[ i ]
+      let pathPart = pathParts[ i ]
 
       if (matchTrailingPathParts === true && parseInt(i) === (matchPart.len - 1)) {
         for (let j in pathParts) {
-          if (j > i) {
-            pathPart = `${pathPart}/${pathParts[ j ]}`;
-          }
+          if (j > i) { pathPart = `${pathPart}/${pathParts[ j ]}` }
         }
       }
 
       if (!pathPart) {
-        return response;
+        return response
       } else if (matchPart[ 0 ] === ':' && matchPart.indexOf('(') < 0) {
-        variable = matchPart.replace(':', '');
+        variable = matchPart.replace(':', '')
         response.params[ variable ] = pathPart
       } else if (matchPart[ 0 ] === ':' && matchPart.indexOf('(') >= 0) {
-        variable = matchPart.replace(':', '').split('(')[ 0 ];
-        regexp = matchPart.substring(matchPart.indexOf('(') + 1, matchPart.length - 1);
-        var matches = pathPart.match(new RegExp(regexp, 'g'));
+        variable = matchPart.replace(':', '').split('(')[ 0 ]
+        regexp = matchPart.substring(matchPart.indexOf('(') + 1, matchPart.length - 1)
+        var matches = pathPart.match(new RegExp(regexp, 'g'))
         if (matches) {
-          response.params[ variable ] = pathPart;
+          response.params[ variable ] = pathPart
         } else {
-          return response;
+          return response
         }
       } else {
         if (pathPart === null || pathPart === undefined || pathParts[ i ].toLowerCase() !== matchPart.toLowerCase()) {
-          return response;
+          return response
         }
       }
     }
 
-    response.match = true;
-    return response;
+    response.match = true
+
+    return response
   }
 
-  registerRoute(method, path, action, apiVersion, matchTrailingPathParts) {
-    let self = this;
-
-    if (!matchTrailingPathParts) {
-      matchTrailingPathParts = false;
-    }
+  /**
+   * Register a new route.
+   *
+   * @param method                    HTTP method
+   * @param path                      URI
+   * @param action                    Action to be executed
+   * @param apiVersion                API version
+   * @param matchTrailingPathParts
+   */
+  registerRoute(method, path, action, apiVersion, matchTrailingPathParts = false) {
+    let self = this
 
     self.routes[ method ].push({
       path: path,
       matchTrailingPathParts: matchTrailingPathParts,
       action: action,
       apiVersion: apiVersion
-    });
+    })
   }
 
+  /**
+   * Load routes.
+   *
+   * @param rawRoutes
+   */
   loadRoutes(rawRoutes) {
-    let self = this;
-    let counter = 0;
+    let self = this
+    let counter = 0
 
-    // define the default routes hash
-    self.routes = {'get': [], 'post': [], 'put': [], 'patch': [], 'delete': []};
-
-    if (!rawRoutes) {
-      if (self.api.config.routes) {
-        rawRoutes = self.api.config.routes;
-      }
-    }
-
-    let v, verb;
+    // iterate all objects
     for (let i in rawRoutes) {
-      let method = i.toLowerCase();
+      // get http method in lower case
+      let method = i.toLowerCase()
 
       for (let j in rawRoutes[ i ]) {
-        let route = rawRoutes[ i ][ j ];
+        let route = rawRoutes[ i ][ j ]
 
         if (method === 'all') {
-          for (v in self.api.routes.verbs) {
-            verb = self.api.routes.verbs[ v ];
-            self.api.routes.registerRoute(verb, route.path, route.action, route.apiVersion, route.matchTrailingPathParts);
-          }
+          // iterate all http methods
+          self.api.routes.verbs.forEach((verb) => {
+            self.api.routes.registerRoute(verb, route.path, route.action, route.apiVersion, route.matchTrailingPathParts)
+          })
         } else {
-          api.routes.registerRoute(method, route.path, route.action, route.apiVersion, route.matchTrailingPathParts);
+          self.api.routes.registerRoute(method, route.path, route.action, route.apiVersion, route.matchTrailingPathParts)
         }
-        counter++;
+        counter++
       }
     }
 
     // remove duplicated entries on postVariables
-    self.api.params.postVariables = Utils.arrayUniqueify(self.api.params.postVariables);
+    self.api.params.postVariables = Utils.arrayUniqueify(self.api.params.postVariables)
 
-    self.api.log(`${counter} routes loaded`, 'debug');
+    // log the number of loaded routes
+    self.api.log(`${counter} routes loaded`, 'debug')
 
     if (self.api.config.servers.web && self.api.config.servers.web.simpleRouting === true) {
-      let simplePaths = [];
+      let simplePaths = []
 
+      // iterate all registered actions
+      self.api.actions.actions.forEach((action) => {
+        // push the action name to the simples paths
+        simplePaths.push(`/${action}`)
 
-      for (let action in self.api.actions.actions) {
-        simplePaths.push(`/${action}`);
-        for (v in self.api.routes.verbs) {
-          verb = self.api.routes.verbs[ v ];
-          self.registerRoute(verb, `/${action}`, action);
-        }
-      }
+        // iterate all verbs
+        self.verbs.forEach((verb) => { self.registerRoute(verb, `/${action}`, action) })
+      })
 
-      self.api.log(`${simplePaths.length} simple routes loaded from action names`, 'debug');
-      self.api.log('routes: ', 'debug', self.api.routes.routes);
+      // log the number of simple routes loaded
+      self.api.log(`${simplePaths.length} simple routes loaded from action names`, 'debug')
+      self.api.log('routes: ', 'debug', self.routes)
     }
+  }
+
+  /**
+   * Load all modules route files.
+   *
+   * If the modules have the 'routes.js' file on the module root
+   * folder we load that file.
+   */
+  loadModulesRoutes() {
+    let self = this
+
+    // iterate all active modules
+    self.api.config.activeModules.forEach((moduleName) => {
+      try {
+        // build the file path
+        let path = `${self.api.scope.rootPath}/modules/${moduleName}/routes.js`
+
+        // check if the module have a 'routes.js' file
+        fs.accessSync(path, fs.F_OK)
+
+        // get the file content
+        let routes = require(path).default
+
+        // load the routes on the engine
+        self.loadRoutes(routes)
+      } catch (e) {
+        // do nothing
+      }
+    })
+
+    // check if we have some routes on the config object
+    if (self.api.config.routes) { self.loadRoutes(self.api.config.routes) }
   }
 }
 
@@ -207,19 +255,28 @@ class RoutesManager {
 export default class {
 
   /**
+   * Initializer load priority.
+   *
    * This only can be loaded after the servers.
    *
    * @type {number}
    */
   static loadPriority = 500;
 
+  /**
+   * Initializer loading function.
+   *
+   * @param api   API reference.
+   * @param next  Callback function.
+   */
   static load(api, next) {
     // put the routes manager available to all platform
     api.routes = new RoutesManager(api);
 
-    // load all routes
-    api.routes.loadRoutes();
+    // load routes from the config file
+    api.routes.loadModulesRoutes();
 
+    // finish the initializer loading
     next();
   }
 
