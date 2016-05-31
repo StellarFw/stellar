0e0903d96bb690261e4cd62dec3abeed9cd97087
Gil Mendes <gil00mendes@gmail.com>
2016-05-31 20:21:59 +0100

    rewrite GenericServer to match the code standard

--- src/GenericServer.js
+++ src/GenericServer.js
@@ -1,11 +1,11 @@
-import {EventEmitter} from 'events';
+import {EventEmitter} from 'events'
 
 /**
  * This function is called when the method is not implemented.
  */
 let methodNotDefined = function () {
-  throw new Error('The containing method should be defined for this server type');
-};
+  throw new Error('The containing method should be defined for this server type')
+}
 
 /**
  * This is the prototypical generic server class that all other types
@@ -13,29 +13,58 @@ let methodNotDefined = function () {
  */
 export default class GenericServer extends EventEmitter {
 
-  api;
-  type;
-  options;
-  attributes;
+  /**
+   * API object reference.
+   */
+  api
 
+  /**
+   * Connection type.
+   */
+  type
+
+  /**
+   * Connection options.
+   */
+  options
+
+  /**
+   * Connection attributes.
+   */
+  attributes
+
+  /**
+   * Constructor.
+   *
+   * @param api
+   * @param name
+   * @param options
+   * @param attributes
+   */
   constructor (api, name, options, attributes) {
+    // call super class constructor
     super();
 
-    this.api = api;
-    this.type = name;
-    this.options = options;
-    this.attributes = attributes;
+    this.api = api
+    this.type = name
+    this.options = options
+    this.attributes = attributes
 
     // attributes can be overwritten by the options
     for (let key in this.options) {
       if (this.attributes[ key ] !== null && this.attributes[ key ] !== undefined) {
-        this.attributes[ key ] = this.options[ key ];
+        this.attributes[ key ] = this.options[ key ]
       }
     }
   }
 
+  /**
+   * Build a new connection object.
+   *
+   * @param data Connection data.
+   */
   buildConnection (data) {
-    let self = this;
+    let self = this
 
     let details = {
       type: self.type,
@@ -43,118 +72,131 @@ export default class GenericServer extends EventEmitter {
       remotePort: data.remotePort,
       remoteIP: data.remoteAddress,
       rawConnection: data.rawConnection
-    };
-
-    if (data.fingerprint) {
-      details.fingerprint = data.fingerprint;
     }
 
-    let connection = new self.api.connection(self.api, details);
+    // if the connection doesn't have a fingerprint already create one
+    if (data.fingerprint) { details.fingerprint = data.fingerprint }
 
-    connection.sendMessage = function (message) {
-      self.sendMessage(connection, message);
-    };
+    // create a new connection instance
+    let connection = new self.api.connection(self.api, details)
 
-    connection.sendFile = function (path) {
-      connection.params.file = path;
-      self.processFile(connection);
-    };
+    // define sendMessage method
+    connection.sendMessage = message => { self.sendMessage(connection, message) }
 
-    this.emit('connection', connection);
-
-    if (this.attributes.logConnections === true) {
-      this.log('new connection', 'info', {to: connection.remoteIP});
+    // define sendFile method
+    connection.sendFile = path => {
+      connection.params.file = path
+      self.processFile(connection)
     }
 
-    if (this.attributes.sendWelcomeMessage === true) {
-      connection.sendMessage({welcome: this.api.config.general.welcomeMessage, context: 'api'});
+    // emit the new connection object
+    self.emit('connection', connection)
+
+    // check if the lod for this type of connection is active
+    if (self.attributes.logConnections === true) { self.log('new connection', 'info', {to: connection.remoteIP}) }
+
+    // bidirectional connection can have a welcome message
+    if (self.attributes.sendWelcomeMessage === true) {
+      connection.sendMessage({welcome: self.api.config.general.welcomeMessage, context: 'api'})
     }
 
-    if (typeof this.attributes.sendWelcomeMessage === 'number') {
-      setTimeout(function () {
+    if (typeof self.attributes.sendWelcomeMessage === 'number') {
+      setTimeout(() => {
         try {
-          connection.sendMessage({welcome: self.api.config.general.welcomeMessage, context: 'api'});
+          connection.sendMessage({welcome: self.api.config.general.welcomeMessage, context: 'api'})
         } catch (e) {
-          self.api.log.error(e);
+          self.api.log.error(e)
         }
-      }, self.attributes.sendWelcomentMessage);
+      }, self.attributes.sendWelcomentMessage)
     }
   }
 
   /**
    * Process an action request.
    *
-   * @param connection
+   * @param connection Connection object.
    */
   processAction (connection) {
-    let self = this;
-    let actionProcessor = new this.api.actionProcessor(self.api, connection, function (data) {
-      self.emit('actionComplete', data);
-    });
+    let self = this
 
-    actionProcessor.processAction();
+    // create a new action processor instance for this request
+    let actionProcessor = new this.api.actionProcessor(self.api, connection, data => {
+      self.emit('actionComplete', data)
+    })
+
+    // process the request
+    actionProcessor.processAction()
   }
 
+  /**
+   * Process a file request.
+   *
+   * @param connection Connection object.
+   */
   processFile (connection) {
-    let self = this;
-    self.api.staticFile.get(connection, function (connection, error, fileStream, mime, length, lastModified) {
-      self.sendFile(connection, error, fileStream, mime, length, lastModified);
-    });
+    let self = this
+
+    self.api.staticFile.get(connection, (connection, error, fileStream, mime, length, lastModified) => {
+      self.sendFile(connection, error, fileStream, mime, length, lastModified)
+    })
   }
 
+  /**
+   * Get all active connection of this server.
+   *
+   * This don't work in some type of servers.
+   *
+   * @returns {Array}
+   */
   connections () {
-    let _connections = [];
+    let _connections = []
 
     for (let i in this.api.connections.connections) {
-      let connection = this.api.connections.connections[ i ];
+      let connection = this.api.connections.connections[ i ]
       if (connection.type === this.type) {
-        _connections.push(connection);
+        _connections.push(connection)
       }
     }
 
-    return _connections;
+    return _connections
   }
 
+  /**
+   * Log function.
+   *
+   * @param message   Message to be logged.
+   * @param severity  Severity level.
+   * @param data      Additional data to be printed out.
+   */
   log (message, severity, data) {
-    switch (severity) {
-      case 'inform':
-        this.api.log.inform(`[Server: ${this.type}] ${message}`, data);
-        break;
-    }
+    let self = this
+    self.api.log(`[Server: ${this.type}] ${message}`, severity, data)
   }
 
   /**
    * Invoked as part of boot.
    */
-  start (next) {
-    methodNotDefined();
-  }
+  start (next) { methodNotDefined() }
 
   /**
-   * Invoked as aprt of shutdown.
+   * Invoked as part of shutdown.
    */
-  stop (next) {
-    methodNotDefined();
-  }
+  stop (next) { methodNotDefined() }
 
   /**
    * This method will be appended to the connection as 'connection.sendMessage'
    *
-   * @param connection
-   * @param message
+   * @param connection  Connection object.
+   * @param message     Message be sent back to the client.
    */
-  sendMessage (connection, message) {
-    methodNotDefined();
-  }
+  sendMessage (connection, message) { methodNotDefined() }
 
   /**
    * This method will be used to gracefully disconnect the client.
    *
-   * @param connection
-   * @param reason
+   * @param connection  Connection object.
+   * @param reason      Reason for disconnection.
    */
-  goodbye (connection, reason) {
-    methodNotDefined();
-  }
+  goodbye (connection, reason) { methodNotDefined() }
 
 }
