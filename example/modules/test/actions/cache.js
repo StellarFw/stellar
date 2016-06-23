3ec5211066362f6b4f4b5df1dd38642becaa3ca5
Gil Mendes <gil00mendes@gmail.com>
2016-06-23 23:11:58 +0100

    update cacheTest code to ES6

--- example/modules/test/actions/cache.js
+++ example/modules/test/actions/cache.js
@@ -1,35 +1,37 @@
-module.exports = [{
+'use strict'
+
+module.exports = [ {
   name: 'cacheTest',
   description: 'I will test the internal cache function of the API',
 
   inputs: {
     key: {
-      require: true
+      required: true
     },
     value: {
-      require: true
+      required: true
     }
   },
 
-  run: function (api, data, next) {
-    var key = 'cache_test_' + data.params.key;
-    var value = data.params.value;
+  run: (api, data, next) => {
+    let key = 'cache_test_' + data.params.key
+    let value = data.params.value
 
     // create the base response object
-    data.response.cacheTestResults = {};
+    data.response.cacheTestResults = {}
 
     // create a new cache entry
-    api.cache.save(key, value, 5000, function (err, resp) {
+    api.cache.save(key, value, 5000, (err, resp) => {
       // append the cache response to the request response
-      data.response.cacheTestResults.saveResp = resp;
+      data.response.cacheTestResults.saveResp = resp
 
       // get the cache size
-      api.cache.size(function(err, numberOfCacheObjects) {
+      api.cache.size((err, numberOfCacheObjects) => {
         // append the cache size to the response object
-        data.response.cacheTestResults.sizeResp = numberOfCacheObjects;
-        
+        data.response.cacheTestResults.sizeResp = numberOfCacheObjects
+
         // load the cache entry
-        api.cache.load(key, function (err, resp, expireTimestamp, createdAt, readAt) {
+        api.cache.load(key, (err, resp, expireTimestamp, createdAt, readAt) => {
           // append the load response to the request response
           data.response.cacheTestResults.loadResp = {
             key: key,
@@ -37,18 +39,18 @@ module.exports = [{
             expireTimestamp: expireTimestamp,
             createdAt: createdAt,
             readAt: readAt
-          };
+          }
 
           // try destroy the cache entry
-          api.cache.destroy(key, function (err, resp) {
+          api.cache.destroy(key, (err, resp) => {
             // append the response to the request response
-            data.response.cacheTestResults.deleteResp = resp;
+            data.response.cacheTestResults.deleteResp = resp
 
             // finish the action execution
-            next();
-          });
-        });
-      });
-    });
+            next()
+          })
+        })
+      })
+    })
   }
-}]
+} ]
