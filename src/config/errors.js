1db3395caec7ba29c7cb541107fd291ea84cee05
Gil Mendes <gil00mendes@gmail.com>
2016-06-29 11:55:30 +0100

    Write errors config to ES6
    And add a new error, verbNotFound

--- src/config/errors.js
+++ src/config/errors.js
@@ -1,7 +1,7 @@
-import Utils from '../utils';
+import Utils from '../utils'
 
 export default {
-  errors: function (api) {
+  errors: api => {
     return {
       '_toExpand': false,
 
@@ -11,28 +11,28 @@ export default {
 
       serializers: {
         servers: {
-          web: function (error) {
+          web: error => {
             if (Utils.isError(error)) {
-              return String(error.message);
+              return String(error.message)
             } else {
-              return error;
+              return error
             }
           },
-          websocket: function (error) {
+          websocket: error => {
             if (Utils.isError(error)) {
-              return String(error.message);
+              return String(error.message)
             } else {
-              return error;
+              return error
             }
           },
-          tcp: function (error) {
+          tcp: error => {
             if (Utils.isError(error)) {
-              return String(error.message);
+              return String(error.message)
             } else {
-              return error;
+              return error
             }
           },
-          helper: function (error) {
+          helper: error => {
             if (Utils.isError(error)) {
               return 'Error: ' + String(error.message)
             } else {
@@ -49,43 +49,43 @@ export default {
       // ---------------------------------------------------------------------
       // The message to accompany general 500 errors (internal server errors)
       // ---------------------------------------------------------------------
-      serverErrorMessage: function () {
-        return 'The server experienced an internal error';
+      serverErrorMessage: () => {
+        return 'The server experienced an internal error'
       },
 
       // ---------------------------------------------------------------------
       // When a params for an action is invalid
       // ---------------------------------------------------------------------
-      invalidParams: function (params) {
-        return params.join(', ');
+      invalidParams: params => {
+        return params.join(', ')
       },
 
       // ---------------------------------------------------------------------
       // When a required param for an action is not provided
       // ---------------------------------------------------------------------
-      missingParams: function (params) {
-        return `${params[ 0 ]} is a required parameter for this action`;
+      missingParams: params => {
+        return `${params[ 0 ]} is a required parameter for this action`
       },
 
       // ---------------------------------------------------------------------
       // user required an unknown action
       // ---------------------------------------------------------------------
-      unknownAction: function (action) {
-        return 'unknown action or invalid apiVersion';
+      unknownAction: action => {
+        return 'unknown action or invalid apiVersion'
       },
 
       // ---------------------------------------------------------------------
       // action can be called by this client/server type
       // ---------------------------------------------------------------------
-      unsupportedServerType: function (type) {
-        return `this action does not support the ${type} connection type`;
+      unsupportedServerType: type => {
+        return `this action does not support the ${type} connection type`
       },
 
       // ---------------------------------------------------------------------
       // Action failed because server is mid-shutdown
       // ---------------------------------------------------------------------
-      serverShuttingDown: function () {
-        return 'the server is shutting down';
+      serverShuttingDown: () => {
+        return 'the server is shutting down'
       },
 
       // ---------------------------------------------------------------------
@@ -94,29 +94,29 @@ export default {
       //
       // the limit is defined in api.config.general.simultaneousActions
       // ---------------------------------------------------------------------
-      tooManyPendingActions: function () {
-        return 'you have too many pending requests';
+      tooManyPendingActions: () => {
+        return 'you have too many pending requests'
       },
 
       // ---------------------------------------------------------------------
       // a poorly designed action cloud try to call next() more than once
       // ---------------------------------------------------------------------
-      doubleCallbackError: function () {
-        return 'Double callback prevented within action';
+      doubleCallbackError: () => {
+        return 'Double callback prevented within action'
       },
 
       // ---------------------------------------------------------------------
       // function to be executed when a file to exists
       // ---------------------------------------------------------------------
-      fileNotFound: function () {
-        return 'The requested file not exists';
+      fileNotFound: () => {
+        return 'The requested file not exists'
       },
 
       // ---------------------------------------------------------------------
       // User didn't request a file
       // ---------------------------------------------------------------------
-      fileNotProvided: function () {
-        return 'File is a required param to send a file';
+      fileNotProvided: () => {
+        return 'File is a required param to send a file'
       },
 
       // ---------------------------------------------------------------------
@@ -124,65 +124,71 @@ export default {
       // ---------------------------------------------------------------------
 
       // ---------------------------------------------------------------------
+      // Function to be executed when a verb is'nt found
+      // ---------------------------------------------------------------------
+      verbNotFound: (connection, verb) => {
+        connection.localize(`the verb non't exists (${verb})`)
+      },
+
+      // ---------------------------------------------------------------------
       // Function to be executed when a verb isn't not allowed
       // ---------------------------------------------------------------------
-      verbNotAllowed: function (connection, verb) {
-        return connection.localize(`verb not found or not allowed (${verb})`);
+      verbNotAllowed: (connection, verb) => {
+        return connection.localize(`verb not found or not allowed (${verb})`)
       },
 
       // ---------------------------------------------------------------------
       // Error handler when the room and the message are not present on the
       // request.
       // ---------------------------------------------------------------------
-      connectionRoomAndMessage: function (connection) {
-        return connection.localize('both room and message are required');
+      connectionRoomAndMessage: connection => {
+        return connection.localize('both room and message are required')
       },
 
       // ---------------------------------------------------------------------
       // Error handle for a request made to a room who the user as not part of
       // ---------------------------------------------------------------------
-      connectionNotInRoom: function (connection, room) {
-        return connection.localize(`connection not in this room (${room})`);
+      connectionNotInRoom: (connection, room) => {
+        return connection.localize(`connection not in this room (${room})`)
       },
 
       // ---------------------------------------------------------------------
       // Error handler for a join request to a room which the user is already
       // part
       // ---------------------------------------------------------------------
-      connectionAlreadyInRoom: function (connection, room) {
-        return connection.localize(`connection already in this room (${room})`);
+      connectionAlreadyInRoom: (connection, room) => {
+        return connection.localize(`connection already in this room (${room})`)
       },
 
       // ---------------------------------------------------------------------
       // Error handle request for a deleted room.
       // ---------------------------------------------------------------------
-      connectionRoomHasBeenDeleted: function (room) {
-        return 'this room has been deleted';
+      connectionRoomHasBeenDeleted: room => {
+        return 'this room has been deleted'
       },
 
       // ---------------------------------------------------------------------
       // Error handler for a join request to a not existing room
       // ---------------------------------------------------------------------
-      connectionRoomNotExist: function (room) {
-        return 'room does not exist';
+      connectionRoomNotExist: room => {
+        return 'room does not exist'
       },
 
       // ---------------------------------------------------------------------
       // Error handler for a room creation request with a same name a already
       // existing room
       // ---------------------------------------------------------------------
-      connectionRoomExists: function (room) {
-        return 'room exists';
+      connectionRoomExists: room => {
+        return 'room exists'
       },
 
       // ---------------------------------------------------------------------
       // Error handler for a request who need a room name and that parameter
       // are not present.
       // ---------------------------------------------------------------------
-      connectionRoomRequired: function (room) {
-        return 'a room is required';
+      connectionRoomRequired: room => {
+        return 'a room is required'
       }
-
-    };
+    }
   }
-};
+}
