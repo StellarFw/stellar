62f2e0a6012dceee089459db5eb0f2e982bdace4
ivoribeiro <ivolopesribeiro15@gmail.com>
2016-11-06 05:03:39 +0000

    Issue #31 dockerIt command to generate a dockerfile for the project

---
+++ bin/commands/dockerIt.js
@@ -0,0 +1,49 @@
+'use strict'
+
+// ----------------------------------------------------------------------------------------------------------- [Imports]
+
+let Command = require('../Command')
+let Utils = require('../utils')
+
+// ------------------------------------------------------------------------------------------------------------- [Class]
+
+class dockerItCommand extends Command {
+
+    constructor(args) {
+        // execute the super class constructor method
+        super()
+
+        // define usage
+        this.usage = 'stellar dockerIt'
+
+        // save the parsed console arguments
+        this.args = args
+    }
+
+    /**
+     * Execute the command.
+     *
+     */
+    execute() {
+        console.log(Utils.exists(process.cwd() + '/dockerfile').toString())
+        console.log(process.cwd() + '/dockerfile')
+//see if a dockerfile already exists
+        if (Utils.exists(process.cwd() + '/dockerfile')) {
+            this.printError('A dockerfile already exists')
+            return false
+        }
+
+
+        // create manifest.json file
+        Utils.generateFileFromTemplate('dockerfile', {}, `${process.cwd()}/dockerfile`)
+
+        // print a success message
+        this.printSuccess(`A dockerfile was created in the project root.\nCreate the image with docker build -t <image_name> .\nCreate a container with docker run -t -p 8080:8080 --name <container_name> <image_name> ! 😉 🌟`)
+
+
+    }
+}
+
+// export the function to execute the command
+module.exports = args => (new dockerItCommand(args)).execute()
+
---
+++ bin/templates/dockerfile.txt
@@ -0,0 +1,9 @@
+FROM node
+WORKDIR server
+RUN npm set registry https://registry.npmjs.org/
+RUN npm install -g stellar-fw
+RUN npm install -g babel-polyfill
+RUN npm install -g mocha
+COPY ./ .
+EXPOSE 8080
+ENTRYPOINT ["stellar","run"]
