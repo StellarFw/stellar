43727d3cbc0b770ca019967ede9b6b0616be90ef
Gil Mendes <gil00mendes@gmail.com>
2016-11-19 20:00:03 +0000

    build the new release

--- dist/config/errors.js
+++ dist/config/errors.js
@@ -64,7 +64,9 @@ exports.default = {
       // ---------------------------------------------------------------------
       invalidParams: errors => {
         const response = {};
-        errors.forEach((message, attribute) => response[attribute] = message);
+        errors.forEach((message, attribute) => {
+          response[attribute] = message;
+        });
         return response;
       },
 
