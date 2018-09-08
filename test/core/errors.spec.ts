53f1e1d62f913029d77c1b004f1baf4c8b513a8c
Gil Mendes <gil00mendes@gmail.com>
2018-09-08 17:29:43 +0100

    Convert errors test to Jest

---
+++ test/core/errors.spec.ts
@@ -0,0 +1,33 @@
+import { startEngine } from "../utils";
+import Engine from "../../dist/engine";
+
+let engine: Engine = null;
+let api: any = null;
+
+describe("Core: Errors", () => {
+  beforeAll(async () => {
+    engine = await startEngine();
+    api = engine.api;
+  });
+
+  afterAll(async () => engine.stop());
+
+  test("returns string errors properly", async () => {
+    const response = await api.helpers.runAction("aNotExistingAction");
+    expect(response.error.code).toBe("004");
+  });
+
+  test("returns Error object properly", async () => {
+    api.configs.errors.unknownAction = () => new Error("error test");
+
+    const response = await api.helpers.runAction("aNotExistingAction");
+    expect(response.error).toBe("Error: error test");
+  });
+
+  test("returns generic object properly", async () => {
+    api.configs.errors.unknownAction = () => ({ code: "error160501" });
+
+    const response = await api.helpers.runAction("aNotExistingAction");
+    expect(response.error.code).toBe("error160501");
+  });
+});
