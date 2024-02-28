e2db7a2b7bfd0ae0c96f8ee0c104c21875539c6a
Gil Mendes <gil00mendes@gmail.com>
2024-02-28 19:43:35 +0000

    chore: fix lint issues

--- src/config/models.js
+++ src/config/models.js
@@ -8,63 +8,63 @@
  * By default we use a memory based adapter to make the startup really simple.
  */
 export default {
-  models() {
-    return {
-      _toExpand: false,
+	models() {
+		return {
+			_toExpand: false,
 
-      // -----------------------------------------------------------------------
-      // Object with model system adapters
-      // -----------------------------------------------------------------------
-      adapters: {
-        memory: "sails-disk",
-      },
+			// -----------------------------------------------------------------------
+			// Object with model system adapters
+			// -----------------------------------------------------------------------
+			adapters: {
+				memory: "sails-disk",
+			},
 
-      // -----------------------------------------------------------------------
-      // Object with the active datastores
-      // -----------------------------------------------------------------------
-      datastores: {
-        memory: {
-          adapter: "memory",
-          inMemoryOnly: true,
-        },
-      },
+			// -----------------------------------------------------------------------
+			// Object with the active datastores
+			// -----------------------------------------------------------------------
+			datastores: {
+				memory: {
+					adapter: "memory",
+					inMemoryOnly: true,
+				},
+			},
 
-      // -----------------------------------------------------------------------
-      // Default models properties based on the Datastore
-      // -----------------------------------------------------------------------
-      // This allows to set default properties for the models. For example, this
-      // is useful when to defined the default primary key, as following.
-      // -----------------------------------------------------------------------
-      defaultModelPropsForDatastore: {
-        memory: {
-          attributes: {
-            id: {
-              type: "number",
-              autoMigrations: {
-                unique: true,
-                autoIncrement: true,
-              },
-            },
-          },
-          primaryKey: "id",
-        },
-      },
+			// -----------------------------------------------------------------------
+			// Default models properties based on the Datastore
+			// -----------------------------------------------------------------------
+			// This allows to set default properties for the models. For example, this
+			// is useful when to defined the default primary key, as following.
+			// -----------------------------------------------------------------------
+			defaultModelPropsForDatastore: {
+				memory: {
+					attributes: {
+						id: {
+							type: "number",
+							autoMigrations: {
+								unique: true,
+								autoIncrement: true,
+							},
+						},
+					},
+					primaryKey: "id",
+				},
+			},
 
-      // -----------------------------------------------------------------------
-      // Default datastore
-      // -----------------------------------------------------------------------
-      defaultDatastore: "memory",
+			// -----------------------------------------------------------------------
+			// Default datastore
+			// -----------------------------------------------------------------------
+			defaultDatastore: "memory",
 
-      // -----------------------------------------------------------------------
-      // Use schemas
-      //
-      // By default Stellar uses a schema based model, this means that only the
-      // defined attributes are inserted on the models.
-      //
-      // You can turn this off when use are using schema-less adapters like the
-      // MongoDB or Redis, if you want.
-      // -----------------------------------------------------------------------
-      schema: true,
-    };
-  },
+			// -----------------------------------------------------------------------
+			// Use schemas
+			//
+			// By default Stellar uses a schema based model, this means that only the
+			// defined attributes are inserted on the models.
+			//
+			// You can turn this off when use are using schema-less adapters like the
+			// MongoDB or Redis, if you want.
+			// -----------------------------------------------------------------------
+			schema: true,
+		};
+	},
 };
