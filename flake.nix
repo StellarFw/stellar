e254967ed0db352371bb2b3b8c2c52374f7df9b9
Gil Mendes <gil00mendes@gmail.com>
2024-10-01 21:29:41 +0100

    chore: add nix flake
    This will allow to have an environment with the tools required for development

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }@inputs:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            corepack_22
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
+}
