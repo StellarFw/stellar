93d956e49cace7f5928ae001c5b4759413d3ae52
Gil Mendes <gil00mendes@gmail.com>
2025-01-22 22:25:14 +0000

    feat: add panic function

---
+++ src/common/fp/runtime/executor.ts
@@ -0,0 +1,9 @@
+/**
+ * Panic with an error.
+ *
+ * The use of this functions isn't recommenced since this can lead to the program to die. Instead it's recommended to
+ * use a Result monad to return an error.
+ */
+export function panic(error: string | Error): never {
+	throw error instanceof Error ? error : Error(error);
+}
