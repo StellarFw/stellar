30d90c76712d1bed65dae4fd15c4038d19071971
Gil Mendes <gil00mendes@gmail.com>
2025-02-20 22:12:23 +0000

    chore: migrate cache tests

--- tests/core/cache.test.ts
+++ tests/core/cache.test.ts
@@ -1,118 +1,118 @@
-import { afterAll, afterEach, beforeAll, describe, it } from "vitest";
-
-import Engine from "../../src/engine";
-import { expect } from "vitest";
-
-const engine = new Engine({ rootPath: `${process.cwd()}/example` });
-
-let api = null;
+import { afterAll, afterEach, beforeAll, describe, test } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+import { buildTestEngine } from "../utils.ts";
+import { isFunction, isObject } from "ramda-adjunct";
 
 describe("Core: Cache", () => {
+	const engine = buildTestEngine();
+
 	beforeAll(async () => {
-		api = await engine.start();
+		await engine.start();
 	});
 
 	afterAll(() => engine.stop());
 
-	it("cache methods should exist", () => {
-		expect(api.cache).toBeTypeOf("object");
-		expect(api.cache.save).toBeTypeOf("function");
-		expect(api.cache.load).toBeTypeOf("function");
-		expect(api.cache.destroy).toBeTypeOf("function");
+	test("cache methods should exist", () => {
+		expect(isObject(engine.api.cache)).toBeTruthy();
+		expect(isFunction(engine.api.cache.save)).toBeTruthy();
+		expect(isFunction(engine.api.cache.load)).toBeTruthy();
+		expect(isFunction(engine.api.cache.destroy)).toBeTruthy();
 	});
 
-	it("cache.save", async () => {
-		await expect(api.cache.save("testKey", "test123", null)).resolves.toBeTruthy();
+	test("cache.save", async () => {
+		await expect(engine.api.cache.save("testKey", "test123", null)).resolves.toBeTruthy();
 	});
 
-	it("cache.load", async () => {
-		await expect(api.cache.load("testKey")).resolves.toMatchObject({
+	test("cache.load", async () => {
+		await expect(engine.api.cache.load("testKey")).resolves.toMatchObject({
 			value: "test123",
 		});
 	});
 
-	it("cache.load failure", async () =>
-		expect(api.cache.load("thisNotExists")).rejects.toThrowError("Object not found"));
-
-	it("cache.destroy", async () => expect(api.cache.destroy("testKey")).resolves.toBeTruthy());
-
-	it("cache.destroy failure", async () => expect(api.cache.destroy("testKey")).resolves.toBeFalsy());
-
-	it("cache.save with expire time", async () => expect(api.cache.save("testKey", "test123", 10)).resolves.toBeTruthy());
-
-	it("cache.load with expired items should not return them", async () => {
-		await expect(api.cache.save("testKeyWait", "test123", 10)).resolves.toBeTruthy();
-
-		await api.utils.delay(20);
-		await expect(api.cache.load("testKeyWait")).rejects.toThrowError("Object expired");
+	test("cache.load failure", async () => {
+		expect(engine.api.cache.load("thisNotExists")).rejects.toThrow("Object not found");
 	});
 
-	it("cache.load with negative expire times will never load", async () => {
-		await expect(api.cache.save("testKeyInThePast", "test123", -1)).resolves.toBeTruthy();
+	test("cache.destroy", async () => expect(engine.api.cache.destroy("testKey")).resolves.toBeTruthy());
 
-		await expect(api.cache.load("testKeyInThePast")).rejects.toThrowError("Object expired");
+	test("cache.destroy failure", async () => expect(engine.api.cache.destroy("testKey")).resolves.toBeFalsy());
+
+	test("cache.save with expire time", async () =>
+		expect(engine.api.cache.save("testKey", "test123", 10)).resolves.toBeTruthy());
+
+	test("cache.load with expired items should not return them", async () => {
+		await expect(engine.api.cache.save("testKeyWait", "test123", 10)).resolves.toBeTruthy();
+
+		await engine.api.utils.delay(20);
+		await expect(engine.api.cache.load("testKeyWait")).rejects.toThrow("Object expired");
 	});
 
-	it("cache.save does not need to pass expireTime", async () => {
-		await expect(api.cache.save("testKeyForNullExpireTime", "test123")).resolves.toBeTruthy();
+	test("cache.load with negative expire times will never load", async () => {
+		await expect(engine.api.cache.save("testKeyInThePast", "test123", -1)).resolves.toBeTruthy();
 
-		await expect(api.cache.load("testKeyForNullExpireTime")).resolves.toMatchObject({ value: "test123" });
+		await expect(engine.api.cache.load("testKeyInThePast")).rejects.toThrow("Object expired");
 	});
 
-	it("cache.load without changing the expireTime will re-apply the redis expire", async () => {
+	test("cache.save does not need to pass expireTime", async () => {
+		await expect(engine.api.cache.save("testKeyForNullExpireTime", "test123")).resolves.toBeTruthy();
+
+		await expect(engine.api.cache.load("testKeyForNullExpireTime")).resolves.toMatchObject({ value: "test123" });
+	});
+
+	test("cache.load without changing the expireTime will re-apply the redis expire", async () => {
 		const key = "testKey";
 
-		await expect(api.cache.save(key, "val", 1000)).resolves.toBeTruthy();
-		await expect(api.cache.load(key)).resolves.toMatchObject({ value: "val" });
+		await expect(engine.api.cache.save(key, "val", 1000)).resolves.toBeTruthy();
+		await expect(engine.api.cache.load(key)).resolves.toMatchObject({ value: "val" });
 
-		await api.utils.delay(1001);
-		await expect(api.cache.load(key)).rejects.toThrowError("Object not found");
+		await engine.api.utils.delay(1001);
+		await expect(engine.api.cache.load(key)).rejects.toThrow("Object not found");
 	});
 
-	it("cache.load with options that extending expireTime should return cached item", async () => {
+	test("cache.load with options that extending expireTime should return cached item", async () => {
 		const timeout = 200;
 		const expireTime = 400;
 		const value = "test123";
 		const key = "testKeyWait";
 
 		// save the initial key
-		await expect(api.cache.save(key, value, expireTime)).resolves.toBeTruthy();
+		await expect(engine.api.cache.save(key, value, expireTime)).resolves.toBeTruthy();
 
 		// wait for `timeout` and try to load the key with a extended expireTime
-		await api.utils.delay(timeout);
+		await engine.api.utils.delay(timeout);
 		await expect(
-			api.cache.load(key, {
+			engine.api.cache.load(key, {
 				expireTimeMS: expireTime,
 			}),
 		).resolves.toMatchObject({ value });
 
 		// wait another `timeout` and load the key again without an extended expire time
-		await api.utils.delay(timeout);
-		await expect(api.cache.load(key)).resolves.toMatchObject({ value });
+		await engine.api.utils.delay(timeout);
+		await expect(engine.api.cache.load(key)).resolves.toMatchObject({ value });
 
 		// wait another `timeout` and the key load should fail without the extended time
-		await api.utils.delay(timeout);
-		await expect(api.cache.load(key)).rejects.toThrowError("Object not found");
+		await engine.api.utils.delay(timeout);
+		await expect(engine.api.cache.load(key)).rejects.toThrow("Object not found");
 	});
 
-	it("cache.save works with arrays", async () => {
-		await expect(api.cache.save("arrayKey", [1, 2, 3])).resolves.toBeTruthy();
+	test("cache.save works with arrays", async () => {
+		await expect(engine.api.cache.save("arrayKey", [1, 2, 3])).resolves.toBeTruthy();
 
-		await expect(api.cache.load("arrayKey")).resolves.toMatchObject({
+		await expect(engine.api.cache.load("arrayKey")).resolves.toMatchObject({
 			value: [1, 2, 3],
 		});
 	});
 
-	it("cache.save works with objects", async () => {
+	test("cache.save works with objects", async () => {
 		const key = "objectKey";
 		const data = {
 			oneThing: "someData",
 			otherThing: [1, 2, 3],
 		};
 
-		await expect(api.cache.save(key, data)).resolves.toBeTruthy();
+		await expect(engine.api.cache.save(key, data)).resolves.toBeTruthy();
 
-		await expect(api.cache.load(key)).resolves.toMatchObject({
+		await expect(engine.api.cache.load(key)).resolves.toMatchObject({
 			value: {
 				oneThing: "someData",
 				otherThing: [1, 2, 3],
@@ -120,42 +120,43 @@ describe("Core: Cache", () => {
 		});
 	});
 
-	it("can clear the cache entirely", async () => {
-		await api.cache.save("cacheClearKey", 123);
+	test("can clear the cache entirely", async () => {
+		await engine.api.cache.save("cacheClearKey", 123);
 
-		await expect(api.cache.size()).resolves.toBeGreaterThan(0);
+		await expect(engine.api.cache.size()).resolves.toBeGreaterThan(0);
 
-		await api.cache.clear();
-		await expect(api.cache.size()).resolves.toBe(0);
+		await engine.api.cache.clear();
+		await expect(engine.api.cache.size()).resolves.toBe(0);
 	});
 
 	describe("lists", () => {
-		it("can push and pop from an array", async () => {
+		test("can push and pop from an array", async () => {
 			const jobs = [];
 
-			jobs.push(api.cache.push("testListKey", "a string"));
-			jobs.push(api.cache.push("testListKey", ["an array"]));
-			jobs.push(api.cache.push("testListKey", { look: "an object" }));
+			jobs.push(engine.api.cache.push("testListKey", "a string"));
+			jobs.push(engine.api.cache.push("testListKey", ["an array"]));
+			jobs.push(engine.api.cache.push("testListKey", { look: "an object" }));
 
 			// process the operations in parallel
 			await expect(Promise.all(jobs)).resolves.toBeDefined();
 
-			expect(api.cache.pop("testListKey")).resolves.toBe("a string");
-			expect(api.cache.pop("testListKey")).resolves.toEqual(["an array"]);
-			expect(api.cache.pop("testListKey")).resolves.toEqual({
+			expect(engine.api.cache.pop("testListKey")).resolves.toBe("a string");
+			expect(engine.api.cache.pop("testListKey")).resolves.toEqual(["an array"]);
+			expect(engine.api.cache.pop("testListKey")).resolves.toEqual({
 				look: "an object",
 			});
 		});
 
-		it("will return null if the list is empty", async () => expect(api.cache.pop("emptyListKey")).resolves.toBeNull());
+		test("will return null if the list is empty", async () =>
+			expect(engine.api.cache.pop("emptyListKey")).resolves.toBeNull());
 
-		it("can get the length of an array when full", async () => {
-			await expect(api.cache.push("testListKeyTwo", "a string")).resolves.toBeDefined();
-			await expect(api.cache.listLength("testListKeyTwo")).resolves.toBe(1);
+		test("can get the length of an array when full", async () => {
+			await expect(engine.api.cache.push("testListKeyTwo", "a string")).resolves.toBeDefined();
+			await expect(engine.api.cache.listLength("testListKeyTwo")).resolves.toBe(1);
 		});
 
-		it("will return 0 length when the key does not exist", async () =>
-			expect(api.cache.listLength("testListKeyNotExists")).resolves.toBe(0));
+		test("will return 0 length when the key does not exist", async () =>
+			expect(engine.api.cache.listLength("testListKeyNotExists")).resolves.toBe(0));
 	});
 
 	describe("locks", function () {
@@ -163,50 +164,52 @@ describe("Core: Cache", () => {
 
 		// reset the lockName and unlock the key after each test
 		afterEach(async () => {
-			api.cache.lockName = api.id;
-			await api.cache.unlock(key);
+			engine.api.cache.lockName = engine.api.id;
+			await engine.api.cache.unlock(key);
 		});
 
-		it("thing can be locked checked and unlocked", async () => {
+		test("thing can be locked checked and unlocked", async () => {
 			// lock a key
-			await expect(api.cache.lock(key, 100)).resolves.toBeTruthy();
+			await expect(engine.api.cache.lock(key, 100)).resolves.toBeTruthy();
 
 			// check the lock
-			await expect(api.cache.checkLock(key, null)).resolves.toBeTruthy();
+			await expect(engine.api.cache.checkLock(key, null)).resolves.toBeTruthy();
 
 			// lock the key
-			await expect(api.cache.unlock(key)).resolves.toBeTruthy();
+			await expect(engine.api.cache.unlock(key)).resolves.toBeTruthy();
 		});
 
-		it("locks have a TTL and the default will be assumed from config", async () => {
+		test("locks have a TTL and the default will be assumed from config", async () => {
 			// lock key
-			await expect(api.cache.lock(key, null)).resolves.toBeTruthy();
+			await expect(engine.api.cache.lock(key, null)).resolves.toBeTruthy();
 
 			// check the lock TTL (Time To Live)
-			await expect(api.redis.clients.client.ttl(api.cache.lockPrefix + key)).resolves.toBeLessThanOrEqual(10);
+			await expect(engine.api.redis.clients.client.ttl(engine.api.cache.lockPrefix + key)).resolves.toBeLessThanOrEqual(
+				10,
+			);
 		});
 
-		it("you can save an item if you do hold the lock", async () => {
-			await expect(api.cache.lock(key, null)).resolves.toBeTruthy();
-			await expect(api.cache.save(key, "value")).resolves.toBeTruthy();
+		test("you can save an item if you do hold the lock", async () => {
+			await expect(engine.api.cache.lock(key, null)).resolves.toBeTruthy();
+			await expect(engine.api.cache.save(key, "value")).resolves.toBeTruthy();
 		});
 
-		it("you cannot save a locked item if you do not hold the lock", async () => {
-			await expect(api.cache.lock(key, null)).resolves.toBeTruthy();
+		test("you cannot save a locked item if you do not hold the lock", async () => {
+			await expect(engine.api.cache.lock(key, null)).resolves.toBeTruthy();
 
 			// change the lock name
-			api.cache.lockName = "otherId";
+			engine.api.cache.lockName = "otherId";
 
-			await expect(api.cache.save(key, "someValue")).rejects.toThrowError("Object locked");
+			await expect(engine.api.cache.save(key, "someValue")).rejects.toThrow("Object locked");
 		});
 
-		it("you cannot destroy a locked item if you do not hold the lock", async () => {
-			await expect(api.cache.lock(key, null)).resolves.toBeTruthy();
+		test("you cannot destroy a locked item if you do not hold the lock", async () => {
+			await expect(engine.api.cache.lock(key, null)).resolves.toBeTruthy();
 
 			// change the lock name
-			api.cache.lockName = "otherId";
+			engine.api.cache.lockName = "otherId";
 
-			await expect(api.cache.destroy(key)).rejects.toThrowError("Object locked");
+			await expect(engine.api.cache.destroy(key)).rejects.toThrow("Object locked");
 		});
 	});
 });
